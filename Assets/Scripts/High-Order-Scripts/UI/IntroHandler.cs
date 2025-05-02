c40021d7157aa127d6e4d7b3fbf0c3d9b9d96801
koko-tsuu <colleen_tan@dlsu.edu.ph>
2025-05-02 14:58:28 +0800

    feat: added intro objective ui animation

---
+++ Assets/Scripts/High-Order-Scripts/UI/IntroHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class IntroHandler : MonoBehaviour
+{
+    [Header("Editable Objectives")]
+    [SerializeField] private string objective;
+    [SerializeField] private int gemAmount;
+
+    [Header("TextMeshProUGUI")]
+    [SerializeField] private TextMeshProUGUI objectiveText;
+    [SerializeField] private TextMeshProUGUI gemText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objective == "")
+        {
+            objective = "Get through the door!";
+        }
+        Time.timeScale = 0;
+        objectiveText.text = "Objective:\n" + objective;
+        gemText.text = "Gems to collect: " + gemAmount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DismissUI()
+    {
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+    }
+}
