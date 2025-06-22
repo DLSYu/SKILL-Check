1d58cc87d1ee300b1a976f07a81e41eb89a7cb21
koko-tsuu <colleen_tan@dlsu.edu.ph>
2025-06-23 07:58:51 +0800

    feat: added intro handler for LOs

---
+++ Assets/Scripts/Low-Order Scripts/IntroHandler_LowOrder.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+
+public class IntroHandler_LowOrder : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+
+    public void DismissUI()
+    {
+        Time.timeScale = 1;
+        this.gameObject.SetActive(false);
+
+
+        Destroy(this.gameObject);
+    }
+}
