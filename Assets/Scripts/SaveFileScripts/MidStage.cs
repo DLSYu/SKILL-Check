32c87500215453896e8cd3c56ed2d4151302c54b
koko-tsuu <colleen_tan@dlsu.edu.ph>
2025-06-29 07:00:19 +0800

    fix: changed rearrangement of the fields in MidStage to be consistent with LateStage

--- Assets/Scripts/SaveFileScripts/MidStage.cs
+++ Assets/Scripts/SaveFileScripts/MidStage.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class MidStage
 {
+    [SerializeField] public List<string> swbstOrFreeformList = new List<string>();
     [SerializeField] public List<HO_SubmitAttempt> attempts = new List<HO_SubmitAttempt>();
     [SerializeField] public List<float> scoresList = new List<float>();
-    [SerializeField] public List<string> swbstOrFreeformList = new List<string>();
+
 }
